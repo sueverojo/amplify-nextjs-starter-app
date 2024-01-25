179904bcebcb43aa01a94410d795f09158f97d35
Antonio de la Torre <antoniosueverojo@gmail.com>
2024-01-25 13:45:43 +0100

    insert and list

--- app/page.tsx
+++ app/page.tsx
@@ -1,105 +1,42 @@
-import Image from 'next/image'
 import styles from './page.module.css'
 
-import { Amplify } from 'aws-amplify';
+import {Amplify} from 'aws-amplify';
 import config from '@/amplifyconfiguration.json';
 
-import { generateClient } from 'aws-amplify/data';
-import type { Schema } from '@/amplify/data/resource'; // Path to your backend resource definition
+import {generateClient} from 'aws-amplify/data';
+import {type Schema} from '@/amplify/data/resource'; // Path to your backend resource definition
 
 Amplify.configure(config);
 
 const client = generateClient<Schema>();
 
+async function insert() {
+    const {errors, data: newTodo} = await client.models.Todo.create({
+        content: "My new todo",
+        isDone: true,
+    })
+}
 // Now you should be able to make CRUDL operations with the
 // Data client
 async function fetchTodos() {
-  const { data: todos, errors } = await client.models.Todo.list();
+    const {data: todos, errors} = await client.models.Todo.list();
+    console.log(todos);
 }
 
+async function main() {
+    console.log("antes")
+    await insert().catch(console.error);
+    await fetchTodos().catch(console.error);
+    console.log("despues")
+}
 
+main();
 
 export default function Home() {
-  return (
-    <main className={styles.main}>
-      <h1>HELLO!</h1>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-      </div>
+    return (
+        <main className={styles.main}>
+            <h1>HELLO!</h1>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-        <span>+</span>
-        <Image
-          src="/amplify.svg"
-          alt="Amplify Logo"
-          width={45}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://docs.amplify.aws/gen2/"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Review documentation for Amplify's code-first DX (Gen 2).</p>
-        </a>
-
-        <a
-          href="https://docs.amplify.aws/gen2/start/quickstart/"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Quickstart <span>-&gt;</span>
-          </h2>
-          <p>Follow a tutorial to build a fullstack app with Amplify Gen 2.</p>
-        </a>
-
-        <a
-          href="https://docs.amplify.aws/gen2/build-a-backend/auth/set-up-auth/"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Auth <span>-&gt;</span>
-          </h2>
-          <p>Zero-config Auth UI components with social sign-in and MFA.</p>
-        </a>
-
-        <a
-          href="https://docs.amplify.aws/gen2/build-a-backend/data/set-up-data/"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Data <span>-&gt;</span>
-          </h2>
-          <p>
-            Fully-typed real-time API with NoSQL database.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+        </main>
+    )
 }
